81b2afa504fbd04ac14454db5181ddf362531f6f
Cristina <cristinaponce.designer@gmail.com>
2021-10-25 23:55:26 +0200

    chore: create models and register them

--- gameapi/admin.py
+++ gameapi/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Game, Party, Message
+
+# Register your models here.
+admin.site.register(Game)
+admin.site.register(Party)
+admin.site.register(Message)
